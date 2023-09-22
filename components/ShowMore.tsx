c0002848827418a65db60ef2ae3cda91a93661a4
Alemocar <65803170+Alemocar@users.noreply.github.com>
2023-09-22 13:11:18 -0600

    Client Server Page

---
+++ components/ShowMore.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+
+import { ShowMoreProps } from '@/types'
+import { updateSearchParams } from '@/utils'
+import CustomButton from './CustomButton'
+
+const ShowMore = ({ pageNumber, isNext, setLimit }: ShowMoreProps) => {
+  const handleNavigation = () => {
+    const newLimit = (pageNumber + 1) * 10
+
+    setLimit(newLimit)
+  }
+  return (
+    <div className="flex-center mt-10 w-full gap-5 ">
+      {!isNext && (
+        <CustomButton
+          title="Show More"
+          btnType="button"
+          containerStyles="bg-primary-blue rounded-full text-white"
+          handleClick={handleNavigation}
+        />
+      )}
+    </div>
+  )
+}
+
+export default ShowMore
