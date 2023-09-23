fc2068aa439f2095dbe5279a4617a7208db8e095
Alemocar <65803170+Alemocar@users.noreply.github.com>
2023-09-22 19:20:33 -0600

    .

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -3,9 +3,9 @@
 import { Fragment, useState } from 'react'
 import Image from 'next/image'
 import { Combobox, Transition } from '@headlessui/react'
-import { SearchManuFacturerProps } from '@types'
 
 import { manufacturers } from '@/constants'
+import { SearchManuFacturerProps } from '@/types'
 
 const SearchManufacturer = ({
   manufacturer,
